b0de6913fd57be904cc45a5d7a696f0041ca6fcf
Praewawawa <praewasompou780@gmail.com>
2025-05-15 22:17:59 +0700

    Initial commit

--- lib/screen/mqtt_service.dart
+++ lib/screen/mqtt_service.dart
@@ -1,25 +1,40 @@
 // screen/mqtt_service.dart
-import 'dart:math';
 import 'dart:io';
+import 'dart:math';
 import 'package:intl/intl.dart';
 import 'package:mqtt_client/mqtt_client.dart';
 import 'package:mqtt_client/mqtt_server_client.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:flutter/foundation.dart';
+
+enum WaterLevelStatus { empty, half, full }
 
 class MqttService {
   static final MqttService _instance = MqttService._internal();
   factory MqttService() => _instance;
   MqttService._internal();
 
-  final String server = "e2b1728dd2a940b0af5f00fd181cdd5e.s2.eu.hivemq.cloud";
+  final String server = "49b6aef9efa2446282d4d2b466bafdcd.s1.eu.hivemq.cloud";
   final int port = 8883;
-  final String username = "PutawanKusumot";
-  final String password = "Tawan09052544";
+  final String username = "chemicalspraying";
+  final String password = "@Dm!np159753";
 
   late MqttServerClient client;
+
   double windSpeed = 0.0;
   double battery = 0.0;
   double chemical = 0.0;
-  bool waterLevel = false;
+  WaterLevelStatus waterLevel = WaterLevelStatus.empty;
+  double _distance = 0.0;
+
+  double get distance => _distance;
+  set distance(double value) {
+    _distance = value;
+    onUpdate?.call();
+  }
+
+  ValueNotifier<LatLng?> currentPosition = ValueNotifier(null);
+
   Function()? onUpdate;
 
   bool _connected = false;
@@ -27,27 +42,30 @@ class MqttService {
 
   Future<void> connect() async {
     if (_connected) return;
-    client = MqttServerClient.withPort(server, _generateClientId(), port);
-    client.secure = true;
-    client.securityContext = SecurityContext.defaultContext;
-    client.keepAlivePeriod = 20;
-    client.onDisconnected = _onDisconnected;
-    client.onConnected = _onConnected;
-    client.onSubscribed = (String topic) => print('Subscribed: \$topic');
+
+    client = MqttServerClient.withPort(server, _generateClientId(), port)
+      ..secure = true
+      ..securityContext = SecurityContext.defaultContext
+      ..keepAlivePeriod = 20
+      ..onDisconnected = _onDisconnected
+      ..onConnected = _onConnected
+      ..onSubscribed = (String topic) => print('Subscribed: $topic');
 
     try {
       await client.connect(username, password);
     } catch (e) {
-      print('MQTT Connect error: \$e');
+      print('MQTT Connect error: $e');
       return;
     }
 
     if (client.connectionStatus?.state == MqttConnectionState.connected) {
       _connected = true;
-      _subscribe("Wind", (msg) => _handleWind(msg));
-      _subscribe("battery", (msg) => _handleBattery(msg));
-      _subscribe("waterLevel", (msg) => _handleWaterLevel(msg));
-      _subscribe("chemical", (msg) => _handleChemical(msg));
+      _subscribe("Wind", _handleWind);
+      _subscribe("battery", _handleBattery);
+      _subscribe("waterLevel", _handleWaterLevel);
+      _subscribe("chemical", _handleChemical);
+      _subscribe("distance", _handleDistance);
+      _subscribe("currentPosition", _handleCurrentPosition);
     } else {
       client.disconnect();
     }
@@ -55,10 +73,17 @@ class MqttService {
 
   void _subscribe(String topic, Function(String) callback) {
     client.subscribe(topic, MqttQos.atMostOnce);
-    client.updates?.listen((c) {
-      final recMess = c.first.payload as MqttPublishMessage;
-      final msg = MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
-      callback(msg);
+
+    client.updates?.listen((List<MqttReceivedMessage<MqttMessage>> events) {
+      for (var event in events) {
+        final receivedTopic = event.topic;
+        if (receivedTopic == topic) {
+          final recMess = event.payload as MqttPublishMessage;
+          final msg = MqttPublishPayload.bytesToStringAsString(
+              recMess.payload.message);
+          callback(msg);
+        }
+      }
     });
   }
 
@@ -73,7 +98,16 @@ class MqttService {
   }
 
   void _handleWaterLevel(String msg) {
-    waterLevel = (msg == "full");
+    switch (msg.toLowerCase()) {
+      case 'full':
+        waterLevel = WaterLevelStatus.full;
+        break;
+      case 'half':
+        waterLevel = WaterLevelStatus.half;
+        break;
+      default:
+        waterLevel = WaterLevelStatus.empty;
+    }
     onUpdate?.call();
   }
 
@@ -82,18 +116,68 @@ class MqttService {
     onUpdate?.call();
   }
 
+  void _handleDistance(String msg) {
+    distance = double.tryParse(msg) ?? 0.0;
+  }
+
+  void _handleCurrentPosition(String msg) {
+    try {
+      final parts = msg.split(',');
+      if (parts.length == 2) {
+        final lat = double.parse(parts[0]);
+        final lon = double.parse(parts[1]);
+        currentPosition.value = LatLng(lat, lon);
+        onUpdate?.call();
+      }
+    } catch (e) {
+      print('Invalid position: $msg');
+    }
+  }
+
   void publish(String topic, String message) {
+    if (!_connected) {
+      print('MQTT not connected. Cannot publish to $topic');
+      return;
+    }
     final builder = MqttClientPayloadBuilder()..addString(message);
     client.publishMessage(topic, MqttQos.atMostOnce, builder.payload!);
   }
 
+  void publishSprayLevel(int sprayLevel) {
+    const String topic = "sprayLevel";
+    publish(topic, sprayLevel.toString());
+  }
+
+  void publishStartNavigation() {
+    publish("waypoint/start", "start");
+  }
+
+  void publishStopNavigation() {
+    publish("waypoint/start", "stop");
+  }
+
+  void publishReturnHome() {
+    publish("waypoint/home", "return");
+  }
+
+  // ฟังก์ชันเพิ่มพิกัดเป้าหมายใหม่ให้ Pi เคลื่อนที่ไป
+  void publishTargetPosition(LatLng position) {
+    final String topic = "waypoint/target";
+    final String message = "${position.latitude},${position.longitude}";
+    publish(topic, message);
+  }
+
   void _onConnected() => print('MQTT Connected');
-  void _onDisconnected() => _connected = false;
+
+  void _onDisconnected() {
+    _connected = false;
+    print('MQTT Disconnected');
+  }
 
   String _generateClientId() {
     final now = DateTime.now();
     final formatter = DateFormat('yyyyMMddHHmmss');
     final random = Random().nextInt(99999);
-    return 'client-\${formatter.format(now)}-\$random';
+    return 'client-${formatter.format(now)}-$random';
   }
 }
--- lib/screen/nottification.dart
+++ lib/screen/nottification.dart
@@ -1,5 +1,6 @@
 // screen/nottification.dart
 
+
 import 'package:flutter/material.dart';
 import 'package:mqtt_client/mqtt_client.dart';
 import 'package:mqtt_client/mqtt_server_client.dart';
